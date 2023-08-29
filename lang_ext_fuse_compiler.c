267081e836568e1115eb6c81e63ba866e8b9b53d
krisvih32 <132164432+krisvih32@users.noreply.github.com>
2023-08-28 17:54:46 -0700

    Update lang_ext_fuse_compiler.c
    Fix buffer size for large files.

--- lang_ext_fuse_compiler.c
+++ lang_ext_fuse_compiler.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "read.c"
+#include <sys/stat.h>
 int main(int argc, char *argv[]) {
 
     printf("main\n");
@@ -21,7 +22,9 @@ int main(int argc, char *argv[]) {
             while (copy_argc!=1){
                 error = 0;
                 printf("%s index\n%d argc\n", argv[copy_argc], copy_argc);
-                char *data = read(64000/sizeof(char), argv[copy_argc], "r", &error);
+		struct stat info;
+		stat(argv[copy_argc], &info)
+                char *data = read((info.)/sizeof(char), argv[copy_argc], "r", &error);
                 printf("data %s\n",data);
                 if (error==14){
                     printf("%d\n", error);
@@ -38,7 +41,7 @@ int main(int argc, char *argv[]) {
             while (copy_argc!=0){
                 error = 0;
                 printf("%s index\n%d argc\n", argv[copy_argc], copy_argc);
-                char* data = read(64000/sizeof(char), argv[copy_argc], "r", &error);
+                char* data = read((info.st_size*8)/sizeof(char), argv[copy_argc], "r", &error);
                 printf("%s\n", data);
                 free(data);
                 if (error==ENOENT){
