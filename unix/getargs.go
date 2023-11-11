9d92a811e5a1abf1db604a752d509653385db874
krisvih32 <132164432+krisvih32@users.noreply.github.com>
2023-11-11 15:43:40 -0800

    made unix folder

---
+++ unix/getargs.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func getargs(argc int, argv []string) {
+	// var e = make([]int, argc)
+	fmt.Printf("in getargs")
+	fmt.Printf("argc %d\n", argc)
+	switch argc {
+	case 1:
+		{
+			wd, err := os.Getwd()
+			if err != nil {
+				fmt.Printf("%+v\n", err)
+				os.Exit(1)
+			}
+			/*out*/ exec.Command("py", fmt.Sprintf("%s%s", wd, "error.py")).Output()
+			if err != nil {
+				fmt.Printf("error %d error number %o %x  : ", err, err, err)
+				os.Exit(2)
+			}
+			fmt.Printf("case 1: no detectable errors\n")
+		}
+	}
+}
